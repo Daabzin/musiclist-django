20b90065d3b013c1c7974fd421f5567b4fd16903
Daabzin <davi38614@gmail.com>
2025-11-26 00:48:21 -0300

    Adicionando funcionalidades de Editar e Excluir Album

--- albuns/urls.py
+++ albuns/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('novo/<int:artista_id>/', views.NovoAlbumView.as_view(), name='novo_album'),
+    path('delete/<int:id>/', views.DeletarAlbumView.as_view(), name='deletar_album'),
+    path('editar/<int:id>/', views.EditarAlbumView.as_view(), name='editar_album'),
 ]
--- albuns/views.py
+++ albuns/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .forms import AlbumForm
+from albuns.models import Album
 from artistas.models import Artista
 
 class NovoAlbumView(View):
@@ -20,3 +21,38 @@ class NovoAlbumView(View):
             return redirect('detalhes', id=artista_id)
             
         return render(request, 'albuns/form_album.html', {'form': form, 'artista': artista})
+    
+class DeletarAlbumView(View):
+    def get(self, request, id):
+        album = get_object_or_404(Album, id=id)
+        return render(request, 'albuns/confirmar_delete_album.html', {'album': album})
+
+    def post(self, request, id):
+        album = get_object_or_404(Album, id=id)
+        artista_id = album.artista.id 
+        album.delete()
+        return redirect('detalhes', id=artista_id) 
+    
+class EditarAlbumView(View):
+    def get(self, request, id):
+        album = get_object_or_404(Album, id=id)
+        form = AlbumForm(instance=album)
+        return render(request, 'albuns/form_album.html', {
+            'form': form, 
+            'artista': album.artista, 
+            'editar': True
+        })
+
+    def post(self, request, id):
+        album = get_object_or_404(Album, id=id)
+        form = AlbumForm(request.POST, instance=album)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('detalhes', id=album.artista.id)
+            
+        return render(request, 'albuns/form_album.html', {
+            'form': form, 
+            'artista': album.artista, 
+            'editar': True
+        })
